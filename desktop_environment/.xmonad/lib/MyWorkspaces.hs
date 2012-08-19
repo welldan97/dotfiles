6fb9492c859411e73f09a330f9011d80633e90f2
Dmitry Yakimov <welldan97@gmail.com>
2012-08-20 00:58:35 +0400

    Extract workspace settings to its own file

---
+++ desktop_environment/.xmonad/lib/MyWorkspaces.hs
@@ -0,0 +1,25 @@
+module MyWorkspaces where
+
+import XMonad
+
+import XMonad.Layout.Grid
+import XMonad.Layout.Maximize
+import XMonad.Layout.NoBorders
+import XMonad.Layout.PerWorkspace
+import XMonad.Layout.ResizableTile
+import XMonad.Layout.Tabbed
+
+myWorkspaces = ["1","2","3","4","5","6","7","8","9","0"]
+
+myLayoutHook  =  onWorkspace "1" full $
+                 onWorkspace "4" web $
+                 onWorkspace "6" web $
+                 onWorkspace "7" chat $
+                 standardLayouts 
+  where
+    standardLayouts = maximize (smartBorders (tiled ||| Mirror tiled ||| Grid ||| simpleTabbed ||| Full)) 
+    web = maximize (smartBorders (tiled ||| simpleTabbed ||| full ))
+    chat = maximize (Grid)
+    full = noBorders Full
+
+    tiled = ResizableTall 1 (2/100) (1/2) []
--- desktop_environment/.xmonad/xmonad.hs
+++ desktop_environment/.xmonad/xmonad.hs
@@ -1,37 +1,14 @@
 import XMonad
-import XMonad.Actions.SpawnOn
-import XMonad.Hooks.ManageDocks
 import XMonad.Hooks.ManageHelpers
-import XMonad.Layout.PerWorkspace
 import XMonad.Util.CustomKeys
 import XMonad.Util.EZConfig
-import XMonad.Layout.Grid
-import XMonad.Layout.IM
-import XMonad.Layout.NoBorders
-import XMonad.Layout.Maximize
-import XMonad.Layout.Reflect
-import XMonad.Layout.ResizableTile
-import XMonad.Layout.Tabbed
-import XMonad.Layout.ThreeColumns
 
 import qualified XMonad.StackSet as W
 import qualified Data.Map        as M
-import Data.Ratio ((%))
 
 import MyKeys
 import MyApplicationsRules
-
-myWorkspaces = ["1","2","3","4","5","6","7","8","9","0"]
-
-myLayoutHook  =  onWorkspace "1" fullL $ onWorkspace "4" webL $ onWorkspace "6" webL $ onWorkspace "7" imLayout $ standardLayouts 
-  where
-    standardLayouts = avoidStruts $ (tiled ||| Mirror tiled ||| Grid ||| simpleTabbed ||| Full) 
-    reflectTiled = reflectHoriz tiled
-    tiled = smartBorders (ResizableTall 1 (2/100) (1/2) [])
-    full = noBorders Full
-    imLayout = Grid
-    webL = avoidStruts $ tiled ||| simpleTabbed ||| full 
-    fullL = avoidStruts $ full
+import MyWorkspaces
 
 modm = mod4Mask
 
