b252e86ed7a07751a8c02d92f10f5dee9bb6543b
Dmitry Yakimov <welldan97@gmail.com>
2012-08-20 00:23:09 +0400

    Added Skype and Pidgin

--- desktop_environment/.xmonad/lib/MyApplicationsRules.hs
+++ desktop_environment/.xmonad/lib/MyApplicationsRules.hs
@@ -9,5 +9,9 @@ import XMonad.Util.EZConfig
 myApplicationsRules = [
   className =? "Do" --> doIgnore,
   className =? "Emacs23" --> doShift "1",
-  className =? "Conkeror" --> doShift "4"
+
+  className =? "Conkeror" --> doShift "4",
+
+  className =? "Pidgin" --> doShift "7",
+  className =? "Skype" --> doShift "7"
   ]
