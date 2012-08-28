a3c82b65f0f887050e3c9d9b4dce56aeae977269
Dmitry Yakimov <welldan97@gmail.com>
2012-08-28 15:15:30 +0400

    Change push-line behavior

--- zsh/.oh-my-zsh/custom/welldan97.zsh
+++ zsh/.oh-my-zsh/custom/welldan97.zsh
@@ -1,4 +1,6 @@
 [[ -s "$HOME/.rvm/scripts/rvm" ]] && . "$HOME/.rvm/scripts/rvm"  # This loads RVM into a shell session.
 
 # open with default opener
-alias o=xdg-open 
+alias o=xdg-open
+
+bindkey '\eq' push-line-or-edit
