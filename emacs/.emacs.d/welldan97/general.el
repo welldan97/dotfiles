c7612008f9da2efe2a5a78a98986c8125b71b1d1
Dmitry Yakimov <welldan97@gmail.com>
2012-09-09 19:54:08 +0400

    Function for simpler emacs configuration reloading

---
+++ emacs/.emacs.d/welldan97/general.el
@@ -0,0 +1,8 @@
+;; Function for simpler emacs configuration reloading
+
+(defun reload-emacs ()
+  "Reload emacs configuration stored in ~/.emacs.d/init.el"
+  (interactive)
+  (load-file "~/.emacs.d/init.el")
+  )
+
