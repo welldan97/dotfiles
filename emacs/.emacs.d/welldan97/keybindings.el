397e1d2056ef45fe3e93a746bf280bb0393936d5
Dmitry Yakimov <welldan97@gmail.com>
2012-09-09 19:53:07 +0400

    Keybindings for navigation between windows

---
+++ emacs/.emacs.d/welldan97/keybindings.el
@@ -0,0 +1,6 @@
+;; Navigation between windows
+
+(global-set-key (kbd "C-S-n") 'windmove-down)
+(global-set-key (kbd "C-S-p") 'windmove-up)
+(global-set-key (kbd "C-S-b") 'windmove-left)
+(global-set-key (kbd "C-S-f") 'windmove-right)
