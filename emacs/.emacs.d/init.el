11596edce02da90e7d0ef10cc2697c6d28fe0be5
Dmitry Yakimov <welldan97@gmail.com>
2012-09-09 17:46:00 +0400

    Add emacs

---
+++ emacs/.emacs.d/init.el
@@ -0,0 +1,22 @@
+;; Needed for Starter Kit installation
+
+(require 'package)
+(add-to-list 'package-archives
+             '("marmalade" . "http://marmalade-repo.org/packages/") t)
+(package-initialize)
+
+(when (not package-archive-contents)
+  (package-refresh-contents))
+
+(defvar my-packages '(starter-kit
+                      starter-kit-lisp
+                      starter-kit-bindings
+                      starter-kit-eshell
+                      starter-kit-js
+                      starter-kit-ruby
+                      starter-kit-lisp)
+  "A list of packages to ensure are installed at launch.")
+
+(dolist (p my-packages)
+  (when (not (package-installed-p p))
+    (package-install p)))
