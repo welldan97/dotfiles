d5293ad2e124726139b2940385ccaca3835ae152
Dmitry Yakimov <welldan97@gmail.com>
2012-09-09 20:22:15 +0400

    Delete backwards with C-h

--- emacs/.emacs.d/welldan97/keybindings.el
+++ emacs/.emacs.d/welldan97/keybindings.el
@@ -4,3 +4,6 @@
 (global-set-key (kbd "C-S-p") 'windmove-up)
 (global-set-key (kbd "C-S-b") 'windmove-left)
 (global-set-key (kbd "C-S-f") 'windmove-right)
+
+;; Delete backwards on C-h
+(global-set-key (kbd "C-h") 'delete-backward-char)
