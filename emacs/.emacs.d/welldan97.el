d07d7bec04e2579447f20868a122b0e2ffdbf75a
Dmitry Yakimov <welldan97@gmail.com>
2012-09-17 15:08:02 +0400

    Delete selected text when yank

--- emacs/.emacs.d/welldan97.el
+++ emacs/.emacs.d/welldan97.el
@@ -7,5 +7,9 @@
 ;; One instance of Emacs
 (server-start)
 
+;; Delete selected text when write to buffer
+(delete-selection-mode)
+
 ;; Operate with current line if no region selected
 (whole-line-or-region-mode)
+
