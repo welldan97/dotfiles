f6bd963c1e79153d845fb66c775752a15b39b09d
Dmitry Yakimov <welldan97@gmail.com>
2012-09-17 14:49:46 +0400

    Change emacs keys

--- emacs/.emacs.d/welldan97/keybindings.el
+++ emacs/.emacs.d/welldan97/keybindings.el
@@ -1,5 +1,8 @@
-;; Navigation between windows
+;; Switch buffers
+(global-set-key (kbd "M-p") 'previous-buffer)
+(global-set-key (kbd "M-n") 'next-buffer)
 
+;; Navigation between windows
 (global-set-key (kbd "C-S-n") 'windmove-down)
 (global-set-key (kbd "C-S-p") 'windmove-up)
 (global-set-key (kbd "C-S-b") 'windmove-left)
@@ -9,5 +12,9 @@
 (global-set-key (kbd "C-h") 'delete-backward-char)
 
 ;; Move text
-(global-set-key (kbd "M-n") 'move-text-down)
-(global-set-key (kbd "M-p") 'move-text-up)
+(global-set-key (kbd "M-N") 'move-text-down)
+(global-set-key (kbd "M-P") 'move-text-up)
+
+;; Multiple line edit
+(global-set-key (kbd "C-S-a") 'mulled/edit-leading-edges)
+(global-set-key (kbd "C-S-e") 'mulled/edit-trailing-edges)
