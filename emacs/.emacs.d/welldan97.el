b69bfcc52f9b15197d79871e4c85689493aa7078
Dmitry Yakimov <welldan97@gmail.com>
2012-09-17 14:43:02 +0400

    Emacs plugins

--- emacs/.emacs.d/welldan97.el
+++ emacs/.emacs.d/welldan97.el
@@ -1,2 +1,8 @@
+;; Load plugins
+(mapc 'load
+      (directory-files
+       (concat user-emacs-directory "welldan97/plugins")
+                       t "^[^#].*el$"))
+
 ;; One instance of Emacs
 (server-start)
