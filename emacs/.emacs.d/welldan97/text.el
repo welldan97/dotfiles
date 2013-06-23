9b8bb77bb802b311301483f996ff84f46198a603
Dmitry Yakimov <welldan97@gmail.com>
2013-06-23 14:42:17 +0700

    Nice font for long texts

--- emacs/.emacs.d/welldan97/text.el
+++ emacs/.emacs.d/welldan97/text.el
@@ -2,3 +2,10 @@
   "Major mode for editing Markdown files" t)
 (setq auto-mode-alist
       (cons '("\\.md" . markdown-mode) auto-mode-alist))
+
+(defun var-font ()
+  "set text font"
+  (interactive)
+  (variable-pitch-mode t)
+  (setq line-spacing 5)
+  )
