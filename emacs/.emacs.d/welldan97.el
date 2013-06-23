c57837a1ce9e9663c4631b67aae969eee255aadd
Dmitry Yakimov <welldan97@gmail.com>
2013-06-23 17:52:32 +0700

    Speedbar

--- emacs/.emacs.d/welldan97.el
+++ emacs/.emacs.d/welldan97.el
@@ -21,3 +21,11 @@
 ;; Snippets
 (setq yas/root-directory "~/.emacs.d/snippets")
 (yas/load-directory yas/root-directory)
+
+;; Speedbar
+(setq speedbar-use-images nil)
+(setq sr-speedbar-right-side nil)
+(setq speedbar-show-unknown-files t)
+(setq speedbar-directory-unshown-regexp "^\\.\\.")
+
+
