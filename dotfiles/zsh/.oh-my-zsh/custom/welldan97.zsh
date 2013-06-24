e4f22c8f7063ee29f0c6cbb6b0219a004346029c
Dmitry Yakimov <welldan97@gmail.com>
2013-06-24 20:58:40 +0700

    Emacs aliases

--- dotfiles/zsh/.oh-my-zsh/custom/welldan97.zsh
+++ dotfiles/zsh/.oh-my-zsh/custom/welldan97.zsh
@@ -1,6 +1,6 @@
 source ~/.oh-my-zsh/custom/product.zsh
 source ~/.oh-my-zsh/custom/system-wide-clipboard.zsh
- 
+
 # This loads RVM into a shell session.
 [[ -s "$HOME/.rvm/scripts/rvm" ]] && . "$HOME/.rvm/scripts/rvm"
 
@@ -15,7 +15,8 @@ alias u=gumba
 
 alias emacs="/Applications/Emacs.app/Contents/MacOS/Emacs"
 alias emacsclient="/Applications/Emacs.app/Contents/MacOS/bin/emacsclient"
-e () { emacsclient "$@" & }
+alias e=emacsclient -t
+alias ec=emacsclient -c
 
 alias gcn!="git commit -v --amend --no-edit"
 alias gcan!="git commit -v -a --amend --no-edit"
@@ -34,7 +35,7 @@ new_rails_app(){
     name=$1
     defaults=${2:-default}
     echo $defaults
-    rails_apps_composer new $name -d ~/.ruby_and_rails/rails_apps_composer/defaults/$defaults.yaml -l ~/.ruby_and_rails/rails_apps_composer/recipes # 
+    rails_apps_composer new $name -d ~/.ruby_and_rails/rails_apps_composer/defaults/$defaults.yaml -l ~/.ruby_and_rails/rails_apps_composer/recipes #
 }
 
 # Keybindings
@@ -42,4 +43,3 @@ bindkey '\eq' push-line-or-edit
 
 # Init Fasd
 eval "$(fasd --init auto)"
-
