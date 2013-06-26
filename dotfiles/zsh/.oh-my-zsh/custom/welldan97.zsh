5b2a9a2e663e4655cbdf1f6dea3e21b34f75a32e
Dmitry Yakimov <welldan97@gmail.com>
2013-06-26 16:53:56 +0700

    Hide into image

--- dotfiles/zsh/.oh-my-zsh/custom/welldan97.zsh
+++ dotfiles/zsh/.oh-my-zsh/custom/welldan97.zsh
@@ -50,3 +50,17 @@ encrypt-dir() {
 decrypt-dir() {
     openssl aes-256-cbc -d -in $1 | tar -x
 }
+
+encrypt-and-hide () {
+    cp $1/image.jpg $(dirname $1)/image.jpg &&
+    encrypt-dir $1 &&
+    zip archive.zip $1.tar.enc &&
+    cat image.jpg archive.zip > $1.jpg &&
+    rm $1.tar.enc image.jpg archive.zip
+}
+
+decrypt-hidden () {
+    unzip $1
+    decrypt-dir $(basename $1 .jpg).tar.enc &&
+    rm $(basename $1 .jpg).tar.enc
+}
