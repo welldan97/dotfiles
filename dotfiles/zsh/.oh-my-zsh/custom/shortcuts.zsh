340120abf5728b339894db8427dcf8a97396ac0f
Dmitry Yakimov <welldan97@gmail.com>
2014-01-25 13:21:36 +0000

    Extract shortcuts

---
+++ dotfiles/zsh/.oh-my-zsh/custom/shortcuts.zsh
@@ -0,0 +1,24 @@
+# One letters
+
+alias o=open
+alias u=gumba
+e() { emacsclient ${1:-'.'} & }
+c() {
+    local prev=$PWD
+    [[ -d "$@" ]] && cd "$@" || z "$@"
+    [[ $PWD != $prev ]] && ls -a
+}
+
+# Git
+
+alias gcn!="git commit -v --amend --no-edit"
+alias gcan!="git commit -v -a --amend --no-edit"
+alias glgo="git log --graph --oneline --all --decorate"
+gp!() { git push $1 $(git rev-parse --abbrev-ref HEAD) }
+
+# Else
+
+alias emacs="/Applications/Emacs.app/Contents/MacOS/Emacs"
+alias zsh-reload='source ~/.zshrc'
+alias source-tree="open -a SourceTree ."
+mkcd () { mkdir -p "$@" && cd "$@" }
