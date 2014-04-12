7bc8249fe17a853b4dca4b0ff7579e29c82ee285
Dmitry Yakimov <welldan97@gmail.com>
2014-04-12 17:46:30 -0500

    Terminal notifier

--- dotfiles/zsh/.oh-my-zsh/custom/shortcuts.zsh
+++ dotfiles/zsh/.oh-my-zsh/custom/shortcuts.zsh
@@ -24,13 +24,15 @@ alias gcan!="git commit -v -a --amend --no-edit"
 alias glgo="git log --graph --oneline --all --decorate"
 gp!() { git push $1 $(git rev-parse --abbrev-ref HEAD) }
 
-# Else
+# Apps
 
 alias emacs="/Applications/Emacs.app/Contents/MacOS/Emacs"
 alias emacsclient="/usr/local/bin/emacsclient"
+alias terminal-notifier="/Applications/terminal-notifier.app/Contents/MacOS/terminal-notifier"
+
+# Else
+
 alias zsh-reload='source ~/.zshrc'
-alias source-tree="open -a SourceTree ."
 
 mkcd() { mkdir -p "$@" && cd "$@" }
 highlight-regexp() { sed "s/$1/\^$fg[blue]$1$fg[default]\$/" }
-reveal() { c $(dirname $1) }
