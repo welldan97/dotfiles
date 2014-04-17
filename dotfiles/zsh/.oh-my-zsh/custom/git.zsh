b57be1a4da039285166166e15c8984d10e08296d
Dmitry Yakimov <welldan97@gmail.com>
2014-04-17 04:15:19 -0500

    Git update and merge master

--- dotfiles/zsh/.oh-my-zsh/custom/git.zsh
+++ dotfiles/zsh/.oh-my-zsh/custom/git.zsh
@@ -2,6 +2,14 @@ alias git-current-branch="git branch | grep '^\*' | sed 's/^\* //'"
 alias git-current-commit="git rev-parse --short HEAD"
 alias git-current-commit-long="git rev-parse HEAD"
 
+git-update-and-merge-master() {
+  branch=$(git-current-branch) &&
+  git checkout master &&
+  git pull &&
+  git checkout $branch &&
+  git merge master
+}
+
 git-remove-from-history() {
     git filter-branch --force --index-filter "git rm -r --cached --ignore-unmatch $1" --prune-empty --tag-name-filter cat -- --all &&
     rm -rf .git/refs/original/ &&
