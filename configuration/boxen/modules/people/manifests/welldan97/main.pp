64e02ede05ec991b41595f83d2e8c181667469fa
Dmitry Yakimov <welldan97@gmail.com>
2015-11-25 02:50:21 +0300

    Move node packages to packages list

--- configuration/boxen/modules/people/manifests/welldan97/main.pp
+++ configuration/boxen/modules/people/manifests/welldan97/main.pp
@@ -39,42 +39,6 @@ class people::welldan97::main {
     version => $people::welldan97::config::node_version
   }
 
-  npm_module { 'npm':
-    module       => 'npm',
-    version      => '~> 3.4',
-    node_version => $people::welldan97::config::node_version
-  }
-
-  npm_module { 'pomodoremi':
-    module       => 'pomodoremi',
-    node_version => $people::welldan97::config::node_version
-  }
-
-  npm_module { 'gulp':
-    module       => 'gulp',
-    node_version => $people::welldan97::config::node_version
-  }
-
-  npm_module { 'mocha':
-    module       => 'mocha',
-    node_version => $people::welldan97::config::node_version
-  }
-
-  npm_module { 'coffee-script':
-    module       => 'coffee-script',
-    node_version => $people::welldan97::config::node_version
-  }
-
-  npm_module { 'js2coffee':
-    module       => 'js2coffee',
-    node_version => $people::welldan97::config::node_version
-  }
-
-  npm_module { 'babel-cli':
-    module       => 'babel-cli',
-    node_version => $people::welldan97::config::node_version
-  }
-
   exec { 'nodenv rehash':
     command => "${boxen::config::home}/nodenv/bin/nodenv rehash",
     require  =>[
--- configuration/boxen/modules/people/manifests/welldan97/packages.pp
+++ configuration/boxen/modules/people/manifests/welldan97/packages.pp
@@ -40,4 +40,40 @@ class people::welldan97::packages {
   }
 
   include people::welldan97::full_contact
+
+  npm_module { 'npm':
+    module       => 'npm',
+    version      => '~> 3.4',
+    node_version => $people::welldan97::config::node_version
+  }
+
+  npm_module { 'pomodoremi':
+    module       => 'pomodoremi',
+    node_version => $people::welldan97::config::node_version
+  }
+
+  npm_module { 'gulp':
+    module       => 'gulp',
+    node_version => $people::welldan97::config::node_version
+  }
+
+  npm_module { 'mocha':
+    module       => 'mocha',
+    node_version => $people::welldan97::config::node_version
+  }
+
+  npm_module { 'coffee-script':
+    module       => 'coffee-script',
+    node_version => $people::welldan97::config::node_version
+  }
+
+  npm_module { 'js2coffee':
+    module       => 'js2coffee',
+    node_version => $people::welldan97::config::node_version
+  }
+
+  npm_module { 'babel-cli':
+    module       => 'babel-cli',
+    node_version => $people::welldan97::config::node_version
+  }
 }
