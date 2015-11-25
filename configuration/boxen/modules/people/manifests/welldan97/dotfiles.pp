2611340bc92df9e5b1b317272e5e559188ce7f86
Dmitry Yakimov <welldan97@gmail.com>
2015-11-26 01:12:39 +0300

    More script installing reorganisation

--- configuration/boxen/modules/people/manifests/welldan97/dotfiles.pp
+++ configuration/boxen/modules/people/manifests/welldan97/dotfiles.pp
@@ -5,17 +5,20 @@ class people::welldan97::dotfiles {
     source => "${::github_user}/dotfiles"
   }
 
+  $dotfiles_install_script_path = './configuration/scripts/install-dotfiles'
+
   exec { "install dotfiles":
     cwd      => $dotfiles_dir,
-    command  => "./install",
+    command  => $dotfiles_install_script_path,
     provider => shell,
     creates  => "${people::welldan97::config::home}/.dotfiles",
+    logoutput => true,
     environment => [
       "DOTFILES_REAL_NAME=${::github_name}",
       "DOTFILES_EMAIL=${::github_email}",
       "HOME=${people::welldan97::config::home}"
-
     ],
+
     require  => [
       Repository["${people::welldan97::config::home}/.emacs.d"],
       Repository[$dotfiles_dir],
