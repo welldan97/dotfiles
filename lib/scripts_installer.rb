a3d1a9f8e9ca3aee24560eb12b146144e5b15c65
Dmitry Yakimov <welldan97@gmail.com>
2015-12-13 18:59:35 +0300

    Fix scripts dir

--- lib/scripts_installer.rb
+++ lib/scripts_installer.rb
@@ -1,7 +1,7 @@
 module ScriptsInstaller
   class << self
     def exec
-      FileUtils.ln_s './scripts', "#{ENV['HOME']}/bin"
+      FileUtils.ln_s File.expand_path('./scripts'), "#{ENV['HOME']}/bin"
     end
   end
 end
