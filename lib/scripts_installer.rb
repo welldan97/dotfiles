f7e399c597a26f541ce665a20763f43dd96831aa
Dmitry Yakimov <welldan97@gmail.com>
2015-12-13 20:33:31 +0300

    Fix install-dotfiles

--- lib/scripts_installer.rb
+++ lib/scripts_installer.rb
@@ -1,6 +1,7 @@
 module ScriptsInstaller
   class << self
     def exec
+      return if File.exists? "#{ENV['HOME']}/bin"
       FileUtils.ln_s File.expand_path('./scripts'), "#{ENV['HOME']}/bin"
     end
   end
