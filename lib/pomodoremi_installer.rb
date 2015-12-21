4656ed1bba5b760e4159283c9b10f933947e19af
Dmitry Yakimov <welldan97@gmail.com>
2015-12-21 21:15:15 +0300

    Faster pomodoremi installer

--- lib/pomodoremi_installer.rb
+++ lib/pomodoremi_installer.rb
@@ -1,6 +1,7 @@
 module PomodoremiInstaller
   class << self
     def exec
+      return if File.exists? "#{ENV['HOME']}/.pomodoremi/node_modules"
       `cd #{ENV['HOME']}/.pomodoremi/ && npm install`
     end
   end
