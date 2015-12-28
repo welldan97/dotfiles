a659c07544c6b5219dd193d13fc6acdfbef54118
Dmitry Yakimov <welldan97@gmail.com>
2015-12-28 13:08:14 +0300

    Fix c

--- dotfiles/zsh/.oh-my-zsh/custom/c.zsh
+++ dotfiles/zsh/.oh-my-zsh/custom/c.zsh
@@ -1,14 +1,14 @@
 c() {
   local previous=$PWD
-  if [[ -d "$@" ]]; then
+  if [ -d "$@" ]; then
     cd "$@"
-  elif [[ -f "$@" ]]; then
-    cd $(dirname "$@")
+  elif [ -f "$@" ]; then
+    cd "$(dirname $@)"
   else
     fasd_cd -d "$@"
   fi
 
-  if [[ $PWD != $previous ]]; then
+  if [ "$PWD" != "$previous" ]; then
     ls -a
   fi
 }
