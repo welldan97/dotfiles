3c207c3c9a2d28493a12d5485c7a63ee56165a9a
Dmitry Yakimov <welldan97@gmail.com>
2016-01-02 21:33:26 +0300

    :scissors:

--- configuration/env/public.sh
+++ configuration/env/public.sh
@@ -1,6 +1,9 @@
 public-config-fetch() {
-  cat $DOTFILES_DIR/configuration/env/public.json | jq '.["'"$1"'"]' | sed 's/"//g'
+  cat "$DOTFILES_DIR/configuration/env/public.json" | jq '.["'"$1"'"]' | sed 's/"//g'
 }
 
-export EMACS_CLIENT_PATH=$(public-config-fetch EMACS_CLIENT_PATH)
-export POSTGRES_APP_BIN_PATH=$(public-config-fetch POSTGRES_APP_BIN_PATH)
+export EMACS_CLIENT_PATH
+export POSTGRES_APP_BIN_PATH
+
+EMACS_CLIENT_PATH="$(public-config-fetch EMACS_CLIENT_PATH)"
+POSTGRES_APP_BIN_PATH="$(public-config-fetch POSTGRES_APP_BIN_PATH)"
