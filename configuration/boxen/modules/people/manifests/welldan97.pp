1659a1442056e47ea328579739ed51a48a3ba37b
Dmitry Yakimov <welldan97@gmail.com>
2016-01-04 12:05:01 +0300

    Remove dotfiles instalation from boxen

--- configuration/boxen/modules/people/manifests/welldan97.pp
+++ configuration/boxen/modules/people/manifests/welldan97.pp
@@ -6,6 +6,5 @@ class people::welldan97 {
   include people::welldan97::dotosx
   include people::welldan97::emacs_prelude
   include people::welldan97::oh_my_zsh
-  include people::welldan97::dotfiles
   include people::welldan97::main
 }
