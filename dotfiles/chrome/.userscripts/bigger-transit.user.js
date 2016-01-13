9dfbfee17a7b94dab2dbeceab35d918fdc7cf5a6
Dmitry Yakimov <welldan97@gmail.com>
2016-01-13 08:30:49 +0300

    Add userscript for bigger translate caption

---
+++ dotfiles/chrome/.userscripts/bigger-transit.user.js
@@ -0,0 +1,8 @@
+// ==UserScript==
+// @name         _biggerTranslit
+// @namespace    welldan97
+// @include      *translate.google.com/*
+// ==/UserScript==
+
+var el = document.getElementById('src-translit');
+el.style.fontSize='50px';
