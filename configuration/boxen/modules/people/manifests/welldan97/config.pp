c6ab0585b96d87b4d75faf03f1adc1b2acc2ff76
Dmitry Yakimov <welldan97@gmail.com>
2016-02-05 16:44:45 +0300

    Detach aquarium from boxen

--- configuration/boxen/modules/people/manifests/welldan97/config.pp
+++ configuration/boxen/modules/people/manifests/welldan97/config.pp
@@ -1,8 +1,5 @@
 class people::welldan97::config {
   $home = "/Users/${::boxen_user}"
-  $info = "/Users/${::boxen_user}/main/info"
-  $products = "/Users/${::boxen_user}/main/products"
-  $support = "/Users/${::boxen_user}/main/support"
 
   $ruby_version = '2.3.0-preview1'
   $node_version = '5.1.0'
--- configuration/boxen/modules/people/manifests/welldan97/main.pp
+++ configuration/boxen/modules/people/manifests/welldan97/main.pp
@@ -1,30 +1,4 @@
 class people::welldan97::main {
-
-  # Build dir tree
-
-  # ~/main
-  #   - info
-  #   - products
-  #   - support
-  #     - cloud_storage
-
-  file { [
-    "${people::welldan97::config::info}",
-    "${people::welldan97::config::products}",
-    "${people::welldan97::config::support}"
-  ]:
-    ensure => "directory",
-  }
-
-  file { "${people::welldan97::config::support}/cloud_storage":
-    ensure => 'link',
-    target => "${people::welldan97::config::home}/Google Drive",
-    require => [
-      File["${people::welldan97::config::support}"],
-      Package['google-drive']
-    ]
-  }
-
   # Use zsh
   osx_chsh { $boxen_user:
     shell   => '/bin/zsh',
