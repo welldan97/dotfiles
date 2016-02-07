509ec5578ddff30280046d7068455cd40a0d7df1
Dmitry Yakimov <welldan97@gmail.com>
2016-02-07 21:23:18 +0300

    Iterate over all keys in configs

--- configuration/env/public.sh
+++ configuration/env/public.sh
@@ -2,10 +2,13 @@ public-config-fetch() {
   cat "$DOTFILES_PATH/configuration/env/public.json" | jq '.["'"$1"'"]' | sed 's/"//g'
 }
 
-export EMACS_PATH
-export EMACS_CLIENT_PATH
-export POSTGRES_APP_BIN_PATH
+public-all-keys() {
+  cat "$DOTFILES_PATH/configuration/env/public.json" |
+    jq 'keys[]' |
+    sed -e 's/"//g'
+}
 
-EMACS_PATH="$(public-config-fetch EMACS_PATH)"
-EMACS_CLIENT_PATH="$(public-config-fetch EMACS_CLIENT_PATH)"
-POSTGRES_APP_BIN_PATH="$(public-config-fetch POSTGRES_APP_BIN_PATH)"
+public-all-keys | while read -r key; do
+  eval "export \"$key\""
+  eval "$key=\"\$(public-config-fetch $key)\""
+done
