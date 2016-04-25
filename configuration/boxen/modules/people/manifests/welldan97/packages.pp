6f2aaa3cbaec9b0868b914e0c04d8e11add30c32
Dmitry Yakimov <welldan97@gmail.com>
2016-04-25 23:09:50 +0300

    Dry packages.pp

--- configuration/boxen/modules/people/manifests/welldan97/packages.pp
+++ configuration/boxen/modules/people/manifests/welldan97/packages.pp
@@ -113,196 +113,63 @@ class people::welldan97::packages {
     provider => 'brewcask'
   }
 
+  $npm_modules = [
+    'babel-cli',
+    'bower',
+    'brunch',
+    'coffee-script',
+    'coffeelint',
+    'ember-cli',
+    'grunt-cli',
+    'gulp',
+    'harp',
+    'hexo-cli',
+    'http-console',
+    'is-progressive',
+    'js-beautify',
+    'js2coffee',
+    'live-server',
+    'mocha',
+    'node-inspector',
+    'npm',
+    'npm-check-updates',
+    'npm-registry-client',
+    'osx-app-spaces-manager',
+    'phantomjs',
+    'pomodoremi',
+    'psi',
+    'strongloop',
+    'typescript',
+    'urban',
+    'wallpaper-cli',
+    'yo'
+  ]
 
-  npm_module { 'babel-cli':
-    module       => 'babel-cli',
-    node_version => $people::welldan97::config::node_version
+  $ruby_gems = [
+    'awesome_print',
+    'bundler',
+    'foreman',
+    'hirb',
+    'pry',
+    'pry-remote',
+    'rails',
+    'rubocop',
+    'scss-lint'
+  ]
+
+  define npm_modules_install ($npm_module = $title) {
+    npm_module { $npm_module:
+      module       => $npm_module,
+      node_version => $people::welldan97::config::node_version
+    }
   }
+  npm_modules_install { $npm_modules: }
 
-  npm_module { 'bower':
-    module       => 'bower',
-    node_version => $people::welldan97::config::node_version
+  define ruby_gems_install ($ruby_gem = $title) {
+    ruby_gem { $ruby_gem:
+      gem          => $ruby_gem,
+      ruby_version => $people::welldan97::config::ruby_version
+    }
   }
-
-  npm_module { 'brunch':
-    module       => 'brunch',
-    node_version => $people::welldan97::config::node_version
-  }
-
-  npm_module { 'coffee-script':
-    module       => 'coffee-script',
-    node_version => $people::welldan97::config::node_version
-  }
-
-  npm_module { 'coffeelint':
-    module       => 'coffeelint',
-    node_version => $people::welldan97::config::node_version
-  }
-
-  npm_module { 'ember-cli':
-    module       => 'ember-cli',
-    node_version => $people::welldan97::config::node_version
-  }
-
-  npm_module { 'hexo-cli':
-    module       => 'hexo-cli',
-    node_version => $people::welldan97::config::node_version
-  }
-
-  npm_module { 'js2coffee':
-    module       => 'js2coffee',
-    node_version => $people::welldan97::config::node_version
-  }
-
-  npm_module { 'grunt-cli':
-    module       => 'grunt-cli',
-    node_version => $people::welldan97::config::node_version
-  }
-
-  npm_module { 'gulp':
-    module       => 'gulp',
-    node_version => $people::welldan97::config::node_version
-  }
-
-  npm_module { 'harp':
-    module          => 'harp',
-    node_version => $people::welldan97::config::node_version
-  }
-
-  npm_module { 'http-console':
-    module          => 'http-console',
-    node_version => $people::welldan97::config::node_version
-  }
-
-  npm_module { 'is-progressive':
-    module          => 'is-progressive',
-    node_version => $people::welldan97::config::node_version
-  }
-
-  npm_module { 'js-beautify':
-    module          => 'js-beautify',
-    node_version => $people::welldan97::config::node_version
-  }
-
-  npm_module { 'live-server':
-    module       => 'live-server',
-    node_version => $people::welldan97::config::node_version
-  }
-
-  npm_module { 'mocha':
-    module       => 'mocha',
-    node_version => $people::welldan97::config::node_version
-  }
-
-  npm_module { 'node-inspector':
-    module       => 'node-inspector',
-    node_version => $people::welldan97::config::node_version
-  }
-
-  npm_module { 'npm':
-    module       => 'npm',
-    version      => '~> 3.5',
-    node_version => $people::welldan97::config::node_version
-  }
-
-  npm_module { 'npm-check-updates':
-    module       => 'npm-check-updates',
-    node_version => $people::welldan97::config::node_version
-  }
-
-  npm_module { 'npm-registry-client':
-    module       => 'npm-registry-client',
-    node_version => $people::welldan97::config::node_version
-  }
-
-  npm_module { 'osx-app-spaces-manager':
-    module       => 'osx-app-spaces-manager',
-    node_version => $people::welldan97::config::node_version
-  }
-
-  npm_module { 'phantomjs':
-    module       => 'phantomjs',
-    node_version => $people::welldan97::config::node_version
-  }
-
-  npm_module { 'pomodoremi':
-    module       => 'pomodoremi',
-    node_version => $people::welldan97::config::node_version
-  }
-
-  npm_module { 'psi':
-    module       => 'psi',
-    node_version => $people::welldan97::config::node_version
-  }
-
-  npm_module { 'strongloop':
-    module       => 'strongloop',
-    node_version => $people::welldan97::config::node_version
-  }
-
-  npm_module { 'typescript':
-    module       => 'typescript',
-    node_version => $people::welldan97::config::node_version
-  }
-
-  npm_module { 'urban':
-    module       => 'urban',
-    node_version => $people::welldan97::config::node_version
-  }
-
-  npm_module { 'wallpaper-cli':
-    module       => 'wallpaper-cli',
-    node_version => $people::welldan97::config::node_version
-  }
-
-  npm_module { 'yo':
-    module       => 'yo',
-    node_version => $people::welldan97::config::node_version
-  }
-
-  ruby_gem { 'awesome_print':
-    gem          => 'awesome_print',
-    ruby_version => $people::welldan97::config::ruby_version
-  }
-
-  ruby_gem { 'bundler':
-    gem          => 'bundler',
-    ruby_version => $people::welldan97::config::ruby_version
-  }
-
-  ruby_gem { 'foreman':
-    gem          => 'foreman',
-    ruby_version => $people::welldan97::config::ruby_version
-  }
-
-  ruby_gem { 'hirb':
-    gem          => 'hirb',
-    ruby_version => $people::welldan97::config::ruby_version
-  }
-
-  ruby_gem { 'pry':
-    gem          => 'pry',
-    ruby_version => $people::welldan97::config::ruby_version
-  }
-
-  ruby_gem { 'pry-remote':
-    gem          => 'pry-remote',
-    ruby_version => $people::welldan97::config::ruby_version
-  }
-
-  ruby_gem { 'rails':
-    gem          => 'rails',
-    ruby_version => $people::welldan97::config::ruby_version
-  }
-
-  ruby_gem { 'rubocop':
-    gem          => 'rubocop',
-    ruby_version => $people::welldan97::config::ruby_version
-  }
-
-  ruby_gem { 'scss-lint':
-    gem          => 'scss-lint',
-    ruby_version => $people::welldan97::config::ruby_version
-  }
-
+  ruby_gems_install { $ruby_gems: }
 }
