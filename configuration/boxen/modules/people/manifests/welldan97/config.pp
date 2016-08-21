99492a28cf460a2a5ff26b309bb1f0e6f9978638
Dmitry Yakimov <welldan97@gmail.com>
2016-08-21 10:40:21 -0300

    Update node

--- configuration/boxen/modules/people/manifests/welldan97/config.pp
+++ configuration/boxen/modules/people/manifests/welldan97/config.pp
@@ -4,7 +4,7 @@ class people::welldan97::config {
   $ruby_versions = ['2.2.3', '2.3.0-preview1']
   $ruby_version = $ruby_versions[0]
 
-  $node_version = '5.1.0'
+  $node_version = '6.4.0'
 
   $python_version = '3.5.0'
 }
