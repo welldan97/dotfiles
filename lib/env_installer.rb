48352f7dec639d189dd4d518b69d2d4af6531b87
Dmitry Yakimov <welldan97@gmail.com>
2016-08-21 10:40:29 -0300

    Fix env export

--- lib/env_installer.rb
+++ lib/env_installer.rb
@@ -1,7 +1,11 @@
 module EnvInstaller
   class << self
     def exec
-      `zsh -c env > #{Dir.home}/.env`
+
+      env = ENV.keys.map do |k|
+        "export #{k}=\"#{ENV[k].gsub('"','\"')}\""
+      end.join("\n")
+      File.write("#{Dir.home}/.env", env)
     end
   end
 end
