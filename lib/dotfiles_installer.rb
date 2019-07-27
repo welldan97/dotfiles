86ccd95cf46254934882d16a48f018c520fb863c
Dmitry Yakimov <welldan97@gmail.com>
2019-07-27 14:12:23 +0000

    Disable dotfiles installers
    This needs update

--- lib/dotfiles_installer.rb
+++ lib/dotfiles_installer.rb
@@ -16,9 +16,9 @@ class DotfilesInstaller < DotfilesProcessor
         File.open(destination(file), 'w') do |f|
           f.write ERB.new(File.read(file)).result($config.instance_eval { binding })
         end
-      elsif file =~ /\.js\.coffee/
-        `coffee -bc --no-header -o "#{File.dirname destination(file)}" "#{file}"`
-        FileUtils.mv "#{destination(file)}.js", destination(file)
+      # elsif file =~ /\.js\.coffee/
+        # `coffee -bc --no-header -o "#{File.dirname destination(file)}" "#{file}"`
+        # FileUtils.mv "#{destination(file)}.js", destination(file)
       else
         FileUtils.cp file, destination(file)
       end
--- lib/pomodoremi_installer.rb
+++ lib/pomodoremi_installer.rb
@@ -2,7 +2,7 @@ module PomodoremiInstaller
   class << self
     def exec
       return if File.exists? "#{ENV['HOME']}/.pomodoremi/node_modules"
-      `cd #{ENV['HOME']}/.pomodoremi/ && npm install`
+#      `cd #{ENV['HOME']}/.pomodoremi/ && npm install`
     end
   end
 end
