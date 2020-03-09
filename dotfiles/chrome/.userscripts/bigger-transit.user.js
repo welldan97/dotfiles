309efe1cc491be5864704449e85d04663c5bb8ef
Dmitry Yakimov <welldan97@gmail.com>
2020-03-09 20:32:28 +0000

    Remove outdated dotfiles

--- dotfiles/chrome/.userscripts/bigger-transit.user.js
+++
@@ -1,8 +0,0 @@
-// ==UserScript==
-// @name         _biggerTranslit
-// @namespace    welldan97
-// @include      *translate.google.com/*
-// ==/UserScript==
-
-var el = document.getElementById('src-translit');
-el.style.fontSize='50px';
