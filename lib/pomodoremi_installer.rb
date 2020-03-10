0eeea590db44202c65dccb3923952c9c08f75df6
Dmitry Yakimov <welldan97@gmail.com>
2020-03-10 14:23:17 +0000

    Initial dotfiles rework

--- lib/pomodoremi_installer.rb
+++
@@ -1,8 +0,0 @@
-module PomodoremiInstaller
-  class << self
-    def exec
-      return if File.exists? "#{ENV['HOME']}/.pomodoremi/node_modules"
-#      `cd #{ENV['HOME']}/.pomodoremi/ && npm install`
-    end
-  end
-end
--- lib/scripts_installer.rb
+++
@@ -1,8 +0,0 @@
-module ScriptsInstaller
-  class << self
-    def exec
-      return if File.exists? "#{ENV['HOME']}/bin"
-      FileUtils.ln_s File.expand_path('./scripts'), "#{ENV['HOME']}/bin"
-    end
-  end
-end
